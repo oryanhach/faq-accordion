92c3b9bdbfff9cade7ba45ab84731c8d942e9801
Oryan Hachamoff <oryanhach95@gmail.com>
2025-05-24 10:31:17 +0300

    added final javascript logic

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-// TODO: Select all .accordion__question buttons
-// TODO: Loop through each button and attach click event
-// TODO: On click:
-//    - If this question is already open:
-//        - Hide its answer (add .hidden)
-//        - Change icon to plus
-//    - Else:
-//        - Close any other open answer (add .hidden, reset icon to plus)
-//        - Show this answer (remove .hidden)
-//        - Change icon to minus
-
 const questions = document.querySelectorAll('.accordion__question')
 
 questions.forEach((question) => {
 	question.addEventListener('click', () => {
 		const item = question.closest('.accordion__item')
-		const answer = item.querySelector('.accordion__answer') // grab the answer inside the item
-		console.log(answer.classList) // log the classes
+		const answer = item.querySelector('.accordion__answer')
+		const img = item.querySelector('img')
+
+		// Close all other answers
+		document.querySelectorAll('.accordion__item').forEach((otherItem) => {
+			if (otherItem !== item) {
+				const otherAnswer =
+					otherItem.querySelector('.accordion__answer')
+				const otherImg = otherItem.querySelector('img')
+				otherAnswer.classList.add('hidden')
+				otherImg.src = 'assets/images/icon-plus.svg'
+			}
+		})
 
 		if (answer.classList.contains('hidden')) {
 			answer.classList.remove('hidden') // Show answer
+
+			img.src = 'assets/images/icon-minus.svg'
 		} else {
 			answer.classList.add('hidden') // Hide answer
+
+			img.src = 'assets/images/icon-plus.svg'
 		}
 	})
 })
-
-// left to do:
-//  change the icon when clicked
-//  change the icon of the first at start
-//  when clicked, change the other questions
